17919cd2aa6941fe895436b5e48e0a028516f851
salehlayaly <lsaleh@hcstonline.org>
2018-08-07 13:34:24 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,8 +1,6 @@
 
 # Code your methods below
-def Proper Stance 
-puts "Plant legs far apart bend knees slightly and keep posture loose"
-end 
-Proper Stance 
-def Base Footwork
-puts "Lift right foot"
+def Starting Stance 
+puts ""
+end
+
