f91518be88ae45fe1aefcdbe758e0f3de47eb13d
salehlayaly <lsaleh@hcstonline.org>
2018-08-07 13:39:39 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,6 +1,17 @@
 
 # Code your methods below
-def Starting Stance 
-puts ""
+def starting_stance 
+  puts "Plant legs far apart, bend knees slightly and keep posture loose" 
+end 
+
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  #finish step 3
+end
+
+def skip_step(starting_foot)
+  puts "Lower #{starting_foot} to the ground"
+  puts ""
 end
 
