40abb064068b9604bf57657a0d6720bed1740256
salehlayaly <lsaleh@hcstonline.org>
2018-08-07 13:54:58 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -24,5 +24,6 @@ def bob_the_reins
 end
 
 def lasso 
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left." 
   puts ""
 end
